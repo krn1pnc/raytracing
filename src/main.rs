71c3ca9675396a9c5c91c9827ce4b61a26e7018a
krn1pnc <krn1pnc@protonmail.com>
2022-08-23 12:23:53 +0800

    feat: render a simple ray tracing image

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::{fs::File, io::BufWriter, path::Path};
 
-use raytracing::{scale2rgb, Color};
+use raytracing::{scale2rgb, Color, Point3d, Ray, Vec3d};
+
+fn ray_color(r: Ray) -> Color {
+    let unit_dire = r.dire.unit();
+    let t = 0.5 * (unit_dire.y() + 1.);
+    return Color::new(1., 1., 1.) * (1. - t) + Color::new(0.5, 0.7, 1.0) * t;
+}
 
 fn main() {
     // Image
-    const IMG_WIDTH: u32 = 256;
-    const IMG_HEIGHT: u32 = 256;
+    const ASPECT_RATIO: f32 = 16. / 9.;
+    const IMG_WIDTH: u32 = 400;
+    const IMG_HEIGHT: u32 = (IMG_WIDTH as f32 / ASPECT_RATIO) as u32;
 
     let img_path = Path::new(r"rendered.png");
     let img_file = File::create(img_path).unwrap();
@@ -15,17 +22,26 @@ fn main() {
     let mut img_writer = img_encoder.write_header().unwrap();
     let mut img_data: Vec<u8> = Vec::new();
 
+    // Camera
+    const VP_HEIGHT: f32 = 2.;
+    const VP_WIDTH: f32 = ASPECT_RATIO * VP_HEIGHT;
+    const FOCAL_LEN: f32 = 1.;
+
+    let origin = Point3d::new(0., 0., 0.);
+    let horiz = Vec3d::new(VP_WIDTH, 0., 0.);
+    let verti = Vec3d::new(0., VP_HEIGHT, 0.);
+    let base = origin - horiz / 2. - verti / 2. - Vec3d::new(0., 0., FOCAL_LEN);
+
     // Render
     let pb = indicatif::ProgressBar::new(IMG_HEIGHT.into());
     for j in (0..IMG_HEIGHT).rev() {
         pb.inc(1);
         for i in 0..IMG_WIDTH {
-            let c = Color::new(
-                i as f32 / (IMG_WIDTH - 1) as f32,
-                j as f32 / (IMG_HEIGHT - 1) as f32,
-                0.25,
-            );
-            img_data.extend(scale2rgb(c));
+            let u = i as f32 / (IMG_WIDTH - 1) as f32;
+            let v = j as f32 / (IMG_HEIGHT - 1) as f32;
+            let r = Ray::new(origin, base + horiz * u + verti * v - origin);
+            let rendered = ray_color(r);
+            img_data.extend(scale2rgb(rendered));
         }
     }
 
