575943c0d08fcef1f59afef70d6b3ba6bd32fffb
krn1pnc <krn1pnc@protonmail.com>
2022-08-23 22:08:20 +0800

    feat: antialiasing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{f32::INFINITY, fs::File, io::BufWriter, path::Path, rc::Rc};
 
-use raytracing::{scale2rgb, Color, Hittable, Point3d, Ray, Scene, Sphere, Vec3d};
+use rand::Rng;
+
+use raytracing::{scale2rgb, Camera, Color, Hittable, Point3d, Ray, Scene, Sphere};
 
 fn ray_color(r: &Ray, s: &Scene) -> Color {
     if let Some(rec) = s.hit(r, 0., INFINITY) {
@@ -13,10 +15,14 @@ fn ray_color(r: &Ray, s: &Scene) -> Color {
 }
 
 fn main() {
+    // random generator
+    let mut rng = rand::thread_rng();
+
     // Image
     const ASPECT_RATIO: f32 = 16. / 9.;
     const IMG_WIDTH: u32 = 400;
     const IMG_HEIGHT: u32 = (IMG_WIDTH as f32 / ASPECT_RATIO) as u32;
+    const SAMPLES_PER_PIXEL: u32 = 100;
 
     let img_path = Path::new(r"rendered.png");
     let img_file = File::create(img_path).unwrap();
@@ -32,25 +38,23 @@ fn main() {
     s.add(Rc::new(Sphere::new(Point3d::new(0., -100.5, -1.), 100.)));
 
     // Camera
-    const VP_HEIGHT: f32 = 2.;
-    const VP_WIDTH: f32 = ASPECT_RATIO * VP_HEIGHT;
-    const FOCAL_LEN: f32 = 1.;
-
-    let origin = Point3d::new(0., 0., 0.);
-    let horiz = Vec3d::new(VP_WIDTH, 0., 0.);
-    let verti = Vec3d::new(0., VP_HEIGHT, 0.);
-    let base = origin - horiz / 2. - verti / 2. - Vec3d::new(0., 0., FOCAL_LEN);
+    let cam = Camera::default();
 
     // Render
     let pb = indicatif::ProgressBar::new(IMG_HEIGHT.into());
     for j in (0..IMG_HEIGHT).rev() {
         pb.inc(1);
+        let mut cur_pixel = Color::default();
         for i in 0..IMG_WIDTH {
-            let u = i as f32 / (IMG_WIDTH - 1) as f32;
-            let v = j as f32 / (IMG_HEIGHT - 1) as f32;
-            let r = Ray::new(origin, base + horiz * u + verti * v - origin);
-            let rendered = ray_color(&r, &s);
-            img_data.extend(scale2rgb(rendered));
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let u = (i as f32 + rng.gen::<f32>()) / (IMG_WIDTH - 1) as f32;
+                let v = (j as f32 + rng.gen::<f32>()) / (IMG_HEIGHT - 1) as f32;
+                let r = cam.get_ray(u, v);
+                let rendered = ray_color(&r, &s);
+                cur_pixel = cur_pixel + rendered;
+            }
+            cur_pixel = cur_pixel / SAMPLES_PER_PIXEL as f32;
+            img_data.extend(scale2rgb(cur_pixel));
         }
     }
 
